bc0b33f67afd8aacd9c5e7f5149f0d69fdea32b8
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2024-01-16 14:18:02 +0200

    Created model migrations with factory and seeder

---
+++ app/Constants/MediaType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class MediaType
+{
+    public const IMAGE = 'image';
+    public const VIDEO = 'video';
+
+}
---
+++ app/Constants/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class Role
+{
+    public const ADMIN = 'admin';
+    public const USER = 'user';
+
+}
